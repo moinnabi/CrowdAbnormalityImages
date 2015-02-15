f81d99e54d7df22f2d656494e5d6d82d95e7868f
Mahadyar Ravanbakhsh <mahdyar.ravanbakhsh@comland.si>
2015-02-15 21:42:13 +0100

    Update HOG

--- code/util/extract_automatic_abnormal_patches.m
+++ code/util/extract_automatic_abnormal_patches.m
@@ -6,7 +6,7 @@ kmeans_size = 10;
 
 output_patches_file='patch_table.mat';
 annotation_xml_file = 'annotation2.xml';
-
+load ('patch_table');
 %[ patch_table ] = extract_raw_patchs_auto( annotation_xml_file, false );
 
 %save(output_patches_file,'patch_table');
@@ -20,12 +20,12 @@ for img_no=1:size (patch_table,1)
     for pacth_no=1:size(rect,1)
         pactch = imresize(imcrop(img, rect(pacth_no,:)), [resize_dim resize_dim]);
         [featureVector, hogVisualization] = extractHOGFeatures(pactch);
-        HOG_Index = [HOG_Index ; patch_table{img_no,1}, rect(pacth_no,:)];
+        HOG_Index = [HOG_Index ; cellstr(patch_table{img_no,1}), rect(pacth_no,:)];
         HOG_features = [HOG_features ; featureVector];
     end
 end
 
-save('HOG_features','HOG_features');
+save('HOG_features','HOG_features','HOG_Index');
 
 [lables] = kmeans(HOG_features,kmeans_size);
 
