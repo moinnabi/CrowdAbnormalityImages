e7b183ae7b7291f7f1a3b3d96a9f840248f6c685
Mahadyar Ravanbakhsh <mahdyar.ravanbakhsh@comland.si>
2015-04-06 12:23:07 +0200

    Add intersect similarity count method

--- code/util/caffe_patches_clustring.m
+++ code/util/caffe_patches_clustring.m
@@ -1,23 +1,33 @@
 
 
-clear all;
+%clear all;
 kmeans_size = 20;
 
 load ('features/caffe_patch/patch_table');
 load ('features/caffe_patch/Caffe_features');
 image_count = size(patch_table,1);
-index_vector_labels_per_image = zeros(image_count,kmeans_size);
+index_vector_labels_per_image_bin = zeros(image_count,kmeans_size);
+index_vector_labels_per_image_hist = zeros(image_count,kmeans_size);
 
 
-[lables] = kmeans(Caffe_feats,kmeans_size);
+%[lables] = kmeans(Caffe_feats,kmeans_size);
 %patch_distribution = get_labe_distribution( lables, kmeans_size );
 
 %% Builup an index binary vector image/lable
 for img_idx=1:image_count
     img_name = patch_table{img_idx,1};
     patch_idns = find(ismember(Caffe_Index (:,1),img_name));
-    patch_lables = unique(lables(patch_idns));
-    index_vector_labels_per_image(img_idx,patch_lables)=1;
+    if size(unique(lables(patch_idns)),1)<2
+        lable_hist = [ 0 , unique(lables(patch_idns))];
+        [patch_count,patch_lables]=hist(lables(patch_idns), lable_hist);
+        patch_count = patch_count(2);
+        patch_lables = patch_lables (2);
+    else
+        [patch_count,patch_lables]=hist(lables(patch_idns), unique(lables(patch_idns)));
+    end
+
+    index_vector_labels_per_image_hist(img_idx,patch_lables) = patch_count ;
+    index_vector_labels_per_image_bin(img_idx,patch_lables)=1;
 end
 
 
@@ -25,10 +35,12 @@ similarity_matrix = zeros(image_count,image_count);
 
 for img_idx=1:image_count
     for img_idx2=1:image_count
-        %% All similarites (Include non-member of clusters)
-        similarity_matrix(img_idx,img_idx2)= sum(index_vector_labels_per_image(img_idx,:)== index_vector_labels_per_image(img_idx2,:));
-        %% Being in the same clusters (only ANDs)
+        %% All similarites (Include non-member of clusters "XNOR")
+        %similarity_matrix(img_idx,img_idx2)= sum(index_vector_labels_per_image_bin(img_idx,:)== index_vector_labels_per_image_bin(img_idx2,:));
+        %% Being in the same clusters ("AND")
         %similarity_matrix(img_idx,img_idx2)= sum(index_vector_labels_per_image(img_idx,:) & index_vector_labels_per_image(img_idx2,:));
+        
+        similarity_matrix(img_idx,img_idx2)= sum(min(index_vector_labels_per_image_hist(img_idx,:),index_vector_labels_per_image_hist(img_idx2,:)));
     end
 end
 
--- code/util/hog_patches_clustring.m
+++ code/util/hog_patches_clustring.m
@@ -1,7 +1,7 @@
 
 
-clear all;
-kmeans_size = 20;
+%clear all;
+kmeans_size = 40;
 
 load ('features/hog_patch/patch_table');
 load ('features/hog_patch/HOG_features');
@@ -15,7 +15,14 @@ index_vector_labels_per_image = zeros(image_count,kmeans_size);
 for img_idx=1:image_count
     img_name = patch_table{img_idx,1};
     patch_idns = find(ismember(HOG_Index (:,1),img_name));
-    patch_lables = unique(lables(patch_idns));
+    if size(unique(lables(patch_idns)),1)<2
+        lable_hist = [ 0 , unique(lables(patch_idns))];
+        [patch_count,patch_lables]=hist(lables(patch_idns), lable_hist);
+        patch_count = patch_count(2);
+        patch_lables = patch_lables (2);
+    else
+        [patch_count,patch_lables]=hist(lables(patch_idns), unique(lables(patch_idns)));
+    end
     index_vector_labels_per_image(img_idx,patch_lables)=1;
 end
 
@@ -25,9 +32,11 @@ similarity_matrix = zeros(image_count,image_count);
 for img_idx=1:image_count
     for img_idx2=1:image_count
         %% All similarites (Include non-member of clusters)
-        similarity_matrix(img_idx,img_idx2)= sum(index_vector_labels_per_image(img_idx,:)== index_vector_labels_per_image(img_idx2,:));
+        %similarity_matrix(img_idx,img_idx2)= sum(index_vector_labels_per_image(img_idx,:)== index_vector_labels_per_image(img_idx2,:));
         %% Being in the same clusters (only ANDs)
         %similarity_matrix(img_idx,img_idx2)= sum(index_vector_labels_per_image(img_idx,:) & index_vector_labels_per_image(img_idx2,:));
+        
+        similarity_matrix(img_idx,img_idx2)= sum(min(index_vector_labels_per_image_hist(img_idx,:),index_vector_labels_per_image_hist(img_idx2,:)));
     end
 end
 
